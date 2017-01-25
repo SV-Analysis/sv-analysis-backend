3e77268cfec489831728f2fc23d9371e5cccf3f9
SHEN Qiaomu <SHEN Qiaomu>
2017-01-25 15:01:15 +0800

    dev

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 mongo = PyMongo(app)
 
 # flask_cors: Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
-CORS(app)
+# CORS(app)
 
 from app.routes import index
 

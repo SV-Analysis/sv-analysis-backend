acfd66679c2992092a8a0c0b8a2d143d57b0d59b
SHEN Qiaomu <SHEN Qiaomu>
2017-01-25 14:54:18 +0800

    deploy

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from app import app
 
 # The port number should be the same as the front end
 try:
-    app.run(use_reloader=False, debug=True)
+    app.run(use_reloader=False, debug=True, port=9930)
 except:
     print("Some thing wrong!")

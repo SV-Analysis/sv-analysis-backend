e903f1f4086332949578d7b7a54ae274429b17ec
qmShen <joyshen06@gmail.com>
2017-02-12 18:17:30 +0800

    'dev'

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # flask_cors: Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
-CORS(app)
+# CORS(app)
 
 from app.routes import index
 
